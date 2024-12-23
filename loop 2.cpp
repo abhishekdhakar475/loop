a60e0615bb836809e0a061f8ad02e445f3d2bd2b
abhishekdhakar475 <abhishekdhakar475@gmail.com>
2024-12-23 03:51:38 -0800

    Add files via upload

---
+++ loop 2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+//	int a;
+//	printf("enter valuve\n");
+//	scanf("%d",&a);
+	for (int a=0; a<100;  a++)
+	if(a%2==0)
+	{
+		printf("even:%d",a);
+	}
+	else
+	{
+		printf("odd:%d",a);
+	}
+}
